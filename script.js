e7d674e73961301589158e1ce1ce4625fd9e3a64
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-08-25 12:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function volume_sphere() {
-    //Write your code here
-  
+function volume_sphere(event) {
+	event.preventDefault();
+	// (4/3)*pi*r*r*r
+	const radiusInput = document.getElementById("radius").value;
+
+	const volumeElement = document.getElementById("volume");
+	const radius = parseFloat(radiusInput);
+	if(isNaN(radius) || radius < 0){
+		volumeElement.value = 'NaN';
+		return;
+	}
+
+	const volume = ((4/3) * Math.PI * Math.pow(radius, 3)).toFixed(4);
+
+	
+	document.getElementById("volume").value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
